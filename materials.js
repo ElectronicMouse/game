da308b2c89323957482aff4d6426b8de700e1bc5
ElectronicMouse <54068147+ElectronicMouse@users.noreply.github.com>
2022-12-19 15:41:46 +0100

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,1 +1,2 @@
-export const IRON = {hardness: 4, flexibility: 1, element: metal}
+export const IRON = {hardness: 4, flexibility: 1, element: "metal"};
+export const BAMBOO = {weight: "light", flexibility: 6, toughness: 4, element: "wood"};  
