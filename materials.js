6a759d2244ab3ad2054288a857d8bbac2326c581
ElectronicMouse <ondrav2000@gmail.com>
2022-12-19 18:46:24 +0100

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,3 +1,3 @@
 //actual properties will be different based on balancing in future
-export const IRON = {hardness: 4, flexibility: 1, element: "metal"};
-export const BAMBOO = {weight: "light", flexibility: 6, toughness: 4, element: "wood"};  
+export const IRON = [{hardness: 4, flexibility: 1, element:"metal", type:"metal"}, [false]]
+export const BAMBOO = [{weight: "light", flexibility: 6, toughness: 4, element:"wood", type:"wood"},[false]];  
