ee44fe582a3580353176e59f1039e1203386e9ed
ElectronicMouse <54068147+ElectronicMouse@users.noreply.github.com>
2022-12-20 21:03:09 +0100

    Update materials.js

--- LoC/materials/materials.js
+++ LoC/materials/materials.js
@@ -3,6 +3,6 @@
 export const LIST = ["hardness", "flexibility", "toughness"];
 //LIST_OF_STANDARDIZED_PROPERTIES: 
 export const LIST_STANDARDIZED = ["type", "element", "weight"]
-//materials
+//materials template: export const MATERIAL = [{properties of object}, [craftable: true/false]];
 export const IRON = [{weight:"heavy", element:"metal", hardness: 4, flexibility: 1, type:"metal"}, [false]];
 export const BAMBOO = [{weight: "light", element:"wood", flexibility: 6, toughness: 4, type:"wood"},[false]];
