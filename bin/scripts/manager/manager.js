980e6369c0ab13d042cd82c59441b921ff50b5f7
ElectronicMouse <ondrav2000@gmail.com>
2023-04-23 20:35:10 +0200

    - Added file write function
    It can take multiple data in form of object and update them based on modifiedProps array

--- bin/scripts/manager/manager.js
+++ bin/scripts/manager/manager.js
@@ -4,24 +4,40 @@ class Manager {
 
   read(path, data) {
     fs.readFile(path, (buffer) => {
-        const file = JSON.parse(buffer);
-        if (Array.isArray(data)) {
-          const results = data.reduce((acc, curr) => {
-            if (file.hasOwnProperty(curr)) {
-              acc[curr] = file[curr];
-            }
-            return acc;
-          }, {});
-          return results;
-        } else {
-          return file;
-        }
-      
-    });
-  }
+      const file = JSON.parse(buffer);
+      if (Array.isArray(data)) {
+        const results = data.reduce((acc, curr) => {
+          if (file.hasOwnProperty(curr)) {
+            acc[curr] = file[curr];
+          }
+          return acc;
+        }, {});
+        return results;
+      } else {
+        return file;
+      }
+    }
+  );
+}
+
+  write(path, data, modifiedProps = null) {
+    fs.readFile(path, (buffer) => {
+      const file = JSON.parse(buffer);
+      if (modifiedProps) {
+        modifiedProps.forEach(prop => {
+          if (file.hasOwnProperty(prop)) {
+            file[prop] = data[prop];
+          }
+        })
+      } else {
+        Object.assign(file, data);
+      }
+      fs.writeFile(path, JSON.stringify(file));
+  });
+}
 
 
-    write(oldFilePath, data, newFilePath){
+  write(oldFilePath, data, newFilePath){
         fs.readFile(oldFilePath, (buffer) => {
             const template = JSON.parse(buffer);
             const newData = Object.assign({}, template, data);
@@ -30,7 +46,7 @@ class Manager {
     }
 
 
-    delete(path){fs.unlink(path)}
+  delete(path){fs.unlink(path)}
 
  
 }
